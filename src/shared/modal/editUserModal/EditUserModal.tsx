0a87f32067a068934de56ef915f6feec6aa0699f
Ivan_Prytsko <ivan.prytsko@rosterathletics.com>
2024-01-24 10:42:02 +0100

    SMS-1: redesigned forms on user edit

--- src/shared/modal/editUserModal/EditUserModal.tsx
+++ src/shared/modal/editUserModal/EditUserModal.tsx
@@ -1,81 +1,118 @@
 import { Modal } from "../modal/Modal.tsx";
 import { useModal } from "../ModalProvider.tsx";
 import { UserModel } from "../../models";
-import { useInput } from "../../../hooks";
 import { v4 as uuidv4 } from 'uuid';
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 type EditUserModalParams = { user?: UserModel, saved: Function }
 
+const requiredMessage: string = 'Field is required';
+const emailRegex: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const emailValidationMessage: string = 'Email address is wrong';
+
 export const EditUserModal = ({ user, saved }: EditUserModalParams) => {
   const modal = useModal();
-  const userName = useInput(user?.userName ?? '');
-  const firstName = useInput(user?.firstName ?? '');
-  const lastName = useInput(user?.lastName ?? '');
-  const email = useInput(user?.email ?? '');
-  console.log('renders', userName)
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    setValue,
+    reset,
+    formState: { errors },
+  } = useForm<UserModel>()
+
+  useEffect(() => {
+    if (user) {
+      setValue('userName', user.userName)
+      setValue('firstName', user.firstName)
+      setValue('lastName', user.lastName)
+      setValue('email', user.email)
+    }
+  }, [modal.visible]);
 
   if (!modal.visible) {
     return null; // Deletes html
   }
 
-  const resetForm = () => {
-    userName.reset();
-    firstName.reset();
-    lastName.reset();
-    email.reset();
-  }
-
   const getUserModel = () => {
-    console.log('getModel')
     return {
+      ...getValues(),
       id: user?.id ?? uuidv4(),
-      userName: userName.value,
-      firstName: firstName.value,
-      lastName: lastName.value,
-      email: email.value
     } as UserModel;
   }
 
   const cancel = () => {
-    resetForm();
+    reset();
     modal.close();
   }
 
-  const save = () => {
-    // TODO: issue, after update, I see previous values
+  const onSubmit: SubmitHandler<UserModel> = () => {
     saved(getUserModel());
-    resetForm();
+    reset();
   }
 
+  // TODO: Good to extract errors to component, so as form-controls
+  // TODO: Good to define fields validations if the code
   return (
       <Modal open={modal.visible}>
-        <h2>{user ? 'Update User' : 'Add New User'}</h2>
-        <div className="modal-body">
-          <div className="form-control-group">
-            <div className="form-control">
-              <label htmlFor="user-name">Username</label>
-              <input id="user-name" type="text" {...userName.bind}/>
-            </div>
-            <div className="form-control">
-              <label htmlFor="first-name">FirstName</label>
-              <input id="first-name" type="text" {...firstName.bind}/>
-            </div>
-            <div className="form-control">
-              <label htmlFor="last-name">LastName</label>
-              <input id="last-name" type="text" {...lastName.bind}/>
-            </div>
-            <div className="form-control">
-              <label htmlFor="email">Email</label>
-              <input id="email" type="email" {...email.bind}/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>{user ? 'Update User' : 'Add New User'}</h2>
+          <div className="modal-body">
+            <div className="form-control-group">
+              <div className="form-control">
+                <label htmlFor="user-name">Username</label>
+                <input id="user-name"
+                       type="text"
+                       className={errors.userName && 'invalid'}
+                       {...register('userName', {
+                         required: requiredMessage,
+                       })}/>
+                {errors.userName && <span className='error'>{errors.userName.message}</span>}
+              </div>
+              <div className="form-control">
+                <label htmlFor="first-name">First Name</label>
+                <input id="first-name"
+                       type="text"
+                       className={errors.firstName && 'invalid'}
+                       {...register('firstName', {
+                         required: requiredMessage,
+                       })}/>
+                {errors.firstName && <span className='error'>{errors.firstName.message}</span>}
+              </div>
+              <div className="form-control">
+                <label htmlFor="last-name">Last Name</label>
+                <input id="last-name"
+                       type="text"
+                       className={errors.lastName && 'invalid'}
+                       {...register('lastName', {
+                         required: requiredMessage,
+                       })}/>
+                {errors.lastName && <span className='error'>{errors.lastName.message}</span>}
+              </div>
+              <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input id="email"
+                       type="text"
+                       className={errors.email && 'invalid'}
+                       {...register('email', {
+                         required: requiredMessage,
+                         pattern: {
+                           value: emailRegex,
+                           message: emailValidationMessage
+                         }
+                       })}/>
+                {errors.email && <span className='error'>{errors.email.message}</span>}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="actions">
-          <button className={`btn`} onClick={cancel}>Cancel
-          </button>
-          <button className={`btn btn-primary`} onClick={save}>{user ? 'Update' : 'Add'}</button>
-        </div>
+          <div className="actions">
+            <button className={`btn`} onClick={cancel}>Cancel
+            </button>
+            <button className={`btn btn-primary`} type="submit">{user ? 'Update' : 'Add'}</button>
+          </div>
+        </form>
       </Modal>
   );
 };
--- src/shared/models/user.model.ts
+++ src/shared/models/user.model.ts
@@ -1,9 +1,9 @@
 export interface UserModel {
   id: string;
   userName: string;
-  firstName?: string;
-  lastName?: string;
-  email?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
   // password: string; //TODO:??
   // passcode: string; //TODO:??
 }
